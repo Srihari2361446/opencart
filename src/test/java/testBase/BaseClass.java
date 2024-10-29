0cf41ec5a1f6dee1d13710a2e0bde3b9ad60627d
Srihari <Srihari.N2@cognizant.com>
2024-10-29 17:38:45 +0530

    first commit

---
+++ src/test/java/testBase/BaseClass.java
@@ -0,0 +1,75 @@
+package testBase;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Properties;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.logging.log4j.LogManager;//log4j
+import org.apache.logging.log4j.Logger; //log4j
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+
+public class BaseClass {
+
+	public WebDriver driver;
+	public static Logger logger;
+	public Properties p;
+
+	@BeforeClass
+	@Parameters({ "os", "browser" })
+	public void setup(String os, String br) throws IOException
+
+	{
+		FileReader file = new FileReader("./src//test//resources//config.properties");
+		p = new Properties();
+		p.load(file);
+
+		logger = LogManager.getLogger(this.getClass());// Log4j
+
+		switch (br.toLowerCase()) {
+		case "chrome":
+			driver = new ChromeDriver();
+			break;
+		case "edge":
+			driver = new EdgeDriver();
+			break;
+		default:
+			System.out.println("No matching browser..");
+			return;
+		}
+
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+		driver.get(p.getProperty("appURL"));
+		driver.manage().window().maximize();
+	}
+
+	@AfterClass
+	public void tearDown() {
+		driver.close();
+	}
+
+	public String randomString() {
+		String generatedString = RandomStringUtils.randomAlphabetic(5);
+		return generatedString;
+	}
+
+	public String randomNumber() {
+		String generatedString = RandomStringUtils.randomNumeric(10);
+		return generatedString;
+	}
+
+	public String randomAlphaNumeric() {
+		String str = RandomStringUtils.randomAlphabetic(3);
+		String num = RandomStringUtils.randomNumeric(3);
+
+		return (str + "@" + num);
+	}
+}
